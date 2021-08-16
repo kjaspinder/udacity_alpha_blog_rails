04be24d6ddf50f7bf29254ac3c26b40f39ca30b6
Jaspinder Kaur <jaspinder.kaur028@gmail.com>
2021-08-16 14:31:43 +0400

    add one to many association between user and articles

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,5 @@
 class Article < ApplicationRecord
+  belongs_to :user
   # to make sure article cannot be created without title
   validates :title, presence: true, length: { minimum: 6, maximum: 100}
   validates :description, presence: true , length: { minimum: 10, maximum: 300}
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_16_082044) do
+ActiveRecord::Schema.define(version: 2021_08_16_101636) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "description"
     t.time "created_at"
     t.time "updated_at"
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
